64e6212790f70cf19026944fcb905fe39a6474b1
Tomas Baronas <tomas.baronas@elephant.healthcare>
2021-03-05 10:58:28 +0100

    add files to run

--- integration/wdio.android.local.conf.js
+++ integration/wdio.android.local.conf.js
@@ -7,8 +7,7 @@ const { config } = require('./wdio.shared.conf');
 config.capabilities = [{
   maxInstances: 1,
   platformName: 'Android',
-  deviceName: 'Android Emulator', // Needed to connect to emulator
-  // Change the path below to find the .apk file
+  deviceName: 'emulator-5554',
   app: path.resolve(__dirname, '../android/app/build/outputs/apk/release/app-release-unsigned.apk'),
   automationName: 'UiAutomator2',
 }];
